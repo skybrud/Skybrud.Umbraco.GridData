5f5ee49e69bc10ee54d78d9f96dad07ba7bf7e07
Anders Bjerner <abjerner@skybrud.dk>
2014-09-21 22:50:40 +0200

    Implemented property value converter for the grid

---
+++ src/Skybrud.Umbraco.GridData/GridPropertyValueConverter.cs
@@ -0,0 +1,32 @@
+﻿using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+
+namespace Skybrud.Umbraco.GridData {
+    
+    public class GridPropertyValueConverter : IPropertyValueConverter {
+
+        public bool IsConverter(PublishedPropertyType propertyType) {
+            return propertyType.PropertyEditorAlias == "Umbraco.Grid";
+        }
+
+        public object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview) {
+            return source;
+        }
+
+        public object ConvertSourceToObject(PublishedPropertyType propertyType, object source, bool preview) {
+
+            // Get the value as a string
+            string str = source as string;
+
+            // Deserialize the string
+            return GridDataModel.Deserialize(str) ?? new GridDataModel();
+
+        }
+
+        public object ConvertSourceToXPath(PublishedPropertyType propertyType, object source, bool preview) {
+            return null;
+        }
+    
+    }
+
+}
