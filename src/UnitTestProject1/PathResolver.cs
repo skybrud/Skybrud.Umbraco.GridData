10b7c72ca12cf0a31a8f81933a43ddedf913c3dc
Anders Bjerner <abjerner@skybrud.dk>
2017-01-22 18:21:02 +0100

    Updated the dependency for UmbracoCms.Core to the newest version (v7.5.7)

--- src/UnitTestProject1/PathResolver.cs
+++ src/UnitTestProject1/PathResolver.cs
@@ -5,7 +5,7 @@
         public static string MapPath(string path) {
             
             // TODO: Find the root directory automatically
-            string root = "D:/Repositories/Skybrud.Umbraco.GridData/src/UnitTestProject1";
+            string root = "D:/Repositories/Skybrud.dk/Skybrud.Umbraco.GridData/src/UnitTestProject1";
             
             return path.StartsWith("~/") ? root + path.Substring(1) : path;
         
