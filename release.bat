b0be0887f80b941197434f10fb7723c4a483797a
Anders Bjerner <abjerner@skybrud.dk>
2019-07-13 16:52:26 +0200

    Build stuff

--- release.bat
+++ release.bat
@@ -1,5 +1,2 @@
 @echo off
-cd src/Skybrud.Umbraco.GridData
-"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\msbuild.exe" /t:pack /p:Configuration=Release /p:PackageOutputPath=../../releases/nuget
-cd ../../
-grunt
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" "src/Skybrud.Umbraco.GridData" /t:rebuild /p:Configuration=Release
