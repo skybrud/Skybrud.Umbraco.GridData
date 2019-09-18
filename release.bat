21111ff7f9c64369f2964dfcff8de5efcf9c10dd
Anders Bjerner <abjerner@skybrud.dk>
2019-09-18 23:04:52 +0200

    New beta release :tada:

--- release.bat
+++ release.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" "src/Skybrud.Umbraco.GridData" /t:rebuild /p:Configuration=Release
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" "src/Skybrud.Umbraco.GridData" /t:rebuild /p:BuildTools=1 /p:Configuration=Release
