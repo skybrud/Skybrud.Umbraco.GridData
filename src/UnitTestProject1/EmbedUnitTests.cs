ca082a1baabd6cf3e629674e21426c66758e65bd
Anders Bjerner <abjerner@skybrud.dk>
2019-12-09 21:03:27 +0100

    Added unit tests for embed control value

---
+++ src/UnitTestProject1/EmbedUnitTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using Skybrud.Umbraco.GridData.Values;
+
+// ReSharper disable IsExpressionAlwaysTrue
+// ReSharper disable SuspiciousTypeConversion.Global
+
+namespace UnitTestProject1 {
+
+    [TestClass]
+    public class EmbedUnitTests {
+
+        [TestMethod]
+        public void Legacy() {
+
+            // Load the JSON
+            string json = File.ReadAllText(PathResolver.MapPath("~/Samples/EmbedLegacy.json"));
+
+            // Parse the grid control
+            JObject control = JObject.Parse(json);
+
+            // Get the JToken for the value
+            JToken token = control.GetValue("value");
+
+            GridControlEmbedValue embed = GridControlEmbedValue.Parse(null, token);
+
+            Assert.IsTrue(embed.IsValid);
+            Assert.IsTrue(embed is IHtmlString);
+            Assert.AreEqual(Environment.NewLine, embed.GetSearchableText());
+
+            Assert.AreEqual(0, embed.Width);
+            Assert.AreEqual(0, embed.Height);
+            Assert.AreEqual(null, embed.Url);
+            Assert.AreEqual(null, embed.Info);
+            Assert.AreEqual("<iframe width=\"360\" height=\"203\" src=\"https://www.youtube.com/embed/VTnDYxwhSaI?feature=oembed\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>", embed.Preview + "");
+
+            Assert.AreEqual("<iframe width=\"360\" height=\"203\" src=\"https://www.youtube.com/embed/VTnDYxwhSaI?feature=oembed\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>", embed.HtmlValue + "");
+
+        }
+
+        [TestMethod]
+        public void Embed() {
+
+            // Load the JSON
+            string json = File.ReadAllText(PathResolver.MapPath("~/Samples/Embed.json"));
+
+            // Parse the grid control
+            JObject control = JObject.Parse(json);
+
+            // Get the JToken for the value
+            JToken token = control.GetValue("value");
+
+            GridControlEmbedValue embed = GridControlEmbedValue.Parse(null, token);
+
+            Assert.IsTrue(embed.IsValid);
+            Assert.IsTrue(embed is IHtmlString);
+            Assert.AreEqual(Environment.NewLine, embed.GetSearchableText());
+
+            //Assert.AreEqual(true, embed.Constrain);
+            Assert.AreEqual(360, embed.Width);
+            Assert.AreEqual(240, embed.Height);
+            Assert.AreEqual("https://www.youtube.com/watch?v=VTnDYxwhSaI", embed.Url);
+            Assert.AreEqual("", embed.Info);
+            Assert.AreEqual("<iframe width=\"360\" height=\"203\" src=\"https://www.youtube.com/embed/VTnDYxwhSaI?feature=oembed\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>", embed.Preview + "");
+
+            Assert.AreEqual("<iframe width=\"360\" height=\"203\" src=\"https://www.youtube.com/embed/VTnDYxwhSaI?feature=oembed\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>", embed.HtmlValue + "");
+
+        }
+
+    }
+
+}
--- src/UnitTestProject1/PathResolver.cs
+++ src/UnitTestProject1/PathResolver.cs
@@ -1,14 +1,12 @@
-﻿namespace UnitTestProject1 {
+﻿using System.IO;
+
+namespace UnitTestProject1 {
 
     public static class PathResolver {
 
         public static string MapPath(string path) {
-            
-            // TODO: Find the root directory automatically
-            string root = "D:/Repositories/Skybrud.dk/Skybrud.Umbraco.GridData/src/UnitTestProject1";
-            
+            string root = Path.GetDirectoryName(typeof(PathResolver).Assembly.Location);
             return path.StartsWith("~/") ? root + path.Substring(1) : path;
-        
         }
 
     }
