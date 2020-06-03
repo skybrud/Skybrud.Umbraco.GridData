5311e99e253539801876957885cd32a5eb70714e
Anders Bjerner <abjerner@skybrud.dk>
2020-06-03 20:31:20 +0200

    Umbraco 8 version of the packages no longer sets a custom file version derived from the start date of the project

--- src/Skybrud.Umbraco.GridData/GridUtils.cs
+++ src/Skybrud.Umbraco.GridData/GridUtils.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Reflection;
 using System.Text.RegularExpressions;
 
@@ -26,40 +25,6 @@ namespace Skybrud.Umbraco.GridData {
         public static string GetFileVersion() {
             Assembly assembly = typeof(GridUtils).Assembly;
             return FileVersionInfo.GetVersionInfo(assembly.Location).FileVersion;
-        }
-
-        /// <summary>
-        /// Gets the amount of days between the date of this build and the date the project was started - that is the 30th of July, 2012.
-        /// </summary>
-        public static int GetDaysSinceStart() {
-
-            // Get the third bit as a string
-            string str = GetFileVersion().Split('.')[2];
-
-            // Parse the string into an integer
-            return int.Parse(str);
-
-        }
-
-        /// <summary>
-        /// Gets the date of this build of Skybrud.Social.
-        /// </summary>
-        public static DateTime GetBuildDate() {
-            return new DateTime(2014, 9, 18).AddDays(GetDaysSinceStart());
-        }
-
-        /// <summary>
-        /// Gets the build number of the day. This is mostly used for internal
-        /// purposes to distinguish builds with the same assembly version.
-        /// </summary>
-        public static int GetBuildNumber() {
-
-            // Get the fourth bit as a string
-            string str = GetFileVersion().Split('.')[3];
-
-            // Parse the string into an integer
-            return int.Parse(str);
-
         }
         
         #endregion
