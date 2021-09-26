c95f8d978fda05d55f00942d29c95c15280cf02a
Anders Bjerner <abjerner@skybrud.dk>
2021-09-26 17:25:29 +0200

    Limbo rebranding, build stuff etc.

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 The MIT License (MIT)
 
-Copyright (c) 2020 Skybrud.dk
+Copyright (c) 2021 <a href="https://www.limbo.works/" target="_blank" rel="noopener">Limbo</a>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- release.bat
+++ release.bat
@@ -1,2 +1,3 @@
 @echo off
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\msbuild.exe" "src/Skybrud.Umbraco.GridData" /t:rebuild /t:pack /p:BuildTools=1 /p:Configuration=Release /p:PackageOutputPath=../../releases/nuget
+
+dotnet build src/Skybrud.Umbraco.GridData --configuration Release /t:rebuild /t:pack -p:BuildTools=1 -p:PackageOutputPath=../../releases/nuget
