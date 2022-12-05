5624ebd67e1e72b3455f15d3376b5f22bc2f46c3
Anders Bjerner <abjerner@skybrud.dk>
2022-12-05 19:56:45 +0100

    Build stuff

--- release.bat
+++ release.bat
@@ -1,2 +1,2 @@
 @echo off
-dotnet build src/Skybrud.Umbraco.GridData --configuration Release /t:rebuild /t:pack -p:BuildTools=1 -p:PackageOutputPath=../../releases/nuget
+dotnet build src/Skybrud.Umbraco.GridData --configuration Release /t:rebuild /t:pack -p:PackageOutputPath=../../releases/nuget
